b9c271c4ee63473513673ff70a64b9ee7fcb66ad
Magani Felix <keysindicet@gmail.com>
2016-03-16 16:00:54 +0300

    Initial commit

---
+++ src/com/felixandego/Main.java
@@ -0,0 +1,12 @@
+package com.felixandego;
+
+/**
+ * Single Server Queue Simulation
+ * @author Felix Andego
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	    new SingleServer().launch(); // Launch the Server !!
+    }
+}
